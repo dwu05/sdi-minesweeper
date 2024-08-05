127ea1bc85a9371441830782b42f4fe2c3d78bbb
Daryl Wu <lyrad.wu@gmail.com>
2024-08-05 07:44:02 -0700

    created react app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+const makeRow = () => Array.from(Array(10), () => 'empty');
+
+let minesweeperBoard = Array.from(Array(10), () => makeRow());
+
+minesweeperBoard[3][5] = 'King';
+
+console.log(minesweeperBoard)
+
+return (
+    <>
+    
+    </>
   );
 }
 
